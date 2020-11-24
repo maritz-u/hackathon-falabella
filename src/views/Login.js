285933a6a595161cd93112a03007979876fb00e6
Maritzu Zúñiga <maritzudelosangeles@gmail.com>
2020-11-24 17:46:17 -0300

    primer paso material-ui

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,5 +1,27 @@
 import React, { Component } from "react";
 import { render } from 'react-dom';
+import Button from '@material-ui/core/Button';
+
+// const useStyles = makeStyles((theme) => ({
+//     paper: {
+//       marginTop: theme.spacing(8),
+//       display: 'flex',
+//       flexDirection: 'column',
+//       alignItems: 'center',
+//     },
+//     avatar: {
+//       margin: theme.spacing(1),
+//       backgroundColor: theme.palette.secondary.main,
+//     },
+//     form: {
+//       width: '100%', // Fix IE 11 issue.
+//       marginTop: theme.spacing(1),
+//     },
+//     submit: {
+//       margin: theme.spacing(3, 0, 2),
+//     },
+//   }));
+
 class Formulario extends Component {
   constructor(props) {
     super(props);
@@ -54,6 +76,9 @@ class Formulario extends Component {
           type="submit"
           value="Ingresar"
         ></button>
+        <Button variant="contained" color="primary" disableElevation>
+  Disable elevation
+</Button>
       </form>
     );
   }
