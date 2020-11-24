ed4338b891d2bcbaf4ef9aa54d8138220499d286
Gipsy Martinez <gdmartinez@falabella.cl>
2020-11-24 19:35:01 -0300

    agregando login

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Login from './views/Login';
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import Login from "./views/Login";
+import SignIn from "./views/SignIn";
 
 function App() {
-      return (
-        <BrowserRouter>
-        
-         <Switch>
-           <Route exact path= "/">
-            <Login />
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          {/*<Login />*/}
+          <SignIn />
         </Route>
         {/* <Route exact path="/">
          
       </Route> */}
-        </Switch>
-        </BrowserRouter>
-      );
-    }
+      </Switch>
+    </BrowserRouter>
+  );
+}
 
 export default App;
