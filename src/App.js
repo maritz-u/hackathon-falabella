d35df4967ad5f0b7b010dfb04635c4b182d4f363
Maritzu Zúñiga <maritzudelosangeles@gmail.com>
2020-11-24 15:36:50 -0300

    Configuracion router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from './views/Login';
 
-const App= () => {
-    return(
-        <Login />
-    );
-};
+function App() {
+      return (
+        <BrowserRouter>
+        
+         <Switch>
+           <Route exact path= "/">
+            <Login />
+        </Route>
+        {/* <Route exact path="/">
+         
+      </Route> */}
+        </Switch>
+        </BrowserRouter>
+      );
+    }
 
 export default App;
