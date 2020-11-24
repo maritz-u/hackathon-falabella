e8e1711fd4d54816e98c93cc6333db3508ed8969
Maritzu Zúñiga <66398095+maritz-u@users.noreply.github.com>
2020-11-24 20:33:44 -0300

    agrega login y tema

--- src/App.js
+++ src/App.js
@@ -3,20 +3,19 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 import Login from "./views/Login";
 import SignIn from "./views/SignIn";
+import theme from "./theme/theme";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          {/*<Login />*/}
-          <SignIn />
-        </Route>
-        {/* <Route exact path="/">
-         
-      </Route> */}
-      </Switch>
-    </BrowserRouter>
+    <MuiThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <SignIn />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </MuiThemeProvider>
   );
 }
 
